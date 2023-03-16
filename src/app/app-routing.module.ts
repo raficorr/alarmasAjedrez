594ccc21667f48f5462619ef6a51fd417329cb9f
unknown <rh.rodriguezr1@uniandes.edu.co>
2023-03-16 06:03:25 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RecordatorioComponent } from './recordatorio/recordatorio.component';
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'recordatorio', component: RecordatorioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/recordatorio/recordatorio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recordatorio',
+  templateUrl: './recordatorio.component.html',
+  styleUrls: ['./recordatorio.component.css']
+})
+export class RecordatorioComponent {
+
+}
